2514472987e62bb3c50ab228f7744958a3274190
Andrea Cardy Bailey <andrea.c.bailey@gmail.com>
2019-06-14 13:20:29 -0400

    Sample repository for hosting on Github Pages - Demo for students

---
+++ form.js
@@ -0,0 +1,18 @@
+'use strict';
+/* global $ */
+
+function serializeJson(form) {
+  const formData = new FormData(form);
+  const o = {};
+  formData.forEach((val, name) => o[name] = val);
+  return JSON.stringify(o);
+}
+
+$('#contactForm').submit(event => {
+  event.preventDefault();
+  // These two lines are THE SAME
+  // let formElement = document.querySelector("#contactForm");
+  let formElement = $('#contactForm')[0];
+  // the [0] here selects the native element
+  console.log( serializeJson(formElement) );
+});
