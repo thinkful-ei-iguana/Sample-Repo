e52880c226d1807216ba57116125ced51783665c
Andrea Cardy Bailey <andrea.c.bailey@gmail.com>
2019-10-28 13:29:47 -0400

    Adding aria-live region

--- form.js
+++ form.js
@@ -1,5 +1,6 @@
 'use strict';
 /* global $ */
+let counter = 0;
 
 function serializeJson(form) {
   const formData = new FormData(form);
@@ -10,13 +11,18 @@ function serializeJson(form) {
 
 $('#contactForm').submit(event => {
   event.preventDefault();
+  console.log('h')
+  counter++;
   // These two lines are THE SAME
   // let formElement = document.querySelector("#contactForm");
   let formElement = $('#contactForm')[0];
   // the [0] here selects the native element
-  console.log( serializeJson(formElement) );
+  console.log(serializeJson(formElement));
 
   $('#contactForm').html(`
     <p>Your form submission has been received!</p>
   `);
+  $('.counter').html(counter);
+
+
 });
